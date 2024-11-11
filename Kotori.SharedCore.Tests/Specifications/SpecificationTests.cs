0b37edb823f99f626e40c5a486c52003b433f5d7
kotori <lyrapuff@gmail.com>
2024-11-11 19:54:44 +0300

    Test: or specification

--- Kotori.SharedCore.Tests/Specifications/SpecificationTests.cs
+++ Kotori.SharedCore.Tests/Specifications/SpecificationTests.cs
@@ -21,4 +21,21 @@ public class SpecificationTests
             .Should()
             .Be(isSatisfied);
     }
+    
+    [Theory]
+    [InlineData(true, true, true)]
+    [InlineData(false, true, true)]
+    [InlineData(true, false, true)]
+    [InlineData(false, false, false)]
+    public void OrSpecificationWorks(bool firstValue, bool secondValue, bool isSatisfied)
+    {
+        var first = new TestSpecification(firstValue);
+        var second = new TestSpecification(secondValue);
+        
+        first
+            .Or(second)
+            .IsSatisfied()
+            .Should()
+            .Be(isSatisfied);
+    }
 }
