c81a97a2a882e780f6c9ce0e4815c39d54b43c79
kotori <lyrapuff@gmail.com>
2024-12-02 20:21:16 +0300

    Feat: implicit result type conversions from data and error types

--- Kotori.SharedCore/Result.cs
+++ Kotori.SharedCore/Result.cs
@@ -9,6 +9,9 @@ public abstract record Result<TError> where TError : Error
         
     public static Fail<TError> Fail(TError error)
         => new (error);
+    
+    public static implicit operator Result<TError>(TError error)
+        => new Fail<TError>(error);
 }
 
 public record Ok<TError> : Result<TError> where TError : Error;
@@ -17,13 +20,17 @@ public record Fail<TError>(TError Error) : Result<TError> where TError : Error;
 
 public abstract record Result<TData, TError> where TError : Error
 {
-    public bool IsOk => this is Ok<TData, TError>;
-    
     public static Ok<TData, TError> Ok(TData data)
         => new(data);
     
     public static Fail<TData, TError> Fail(TError error)
         => new (error);
+    
+    public static implicit operator Result<TData, TError>(TData data)
+        => new Ok<TData, TError>(data);
+    
+    public static implicit operator Result<TData, TError>(TError error)
+        => new Fail<TData, TError>(error);
 }
 
 public record Ok<TData, TError>(TData Data) : Result<TData, TError> where TError : Error;
