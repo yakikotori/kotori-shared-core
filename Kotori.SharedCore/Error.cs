36dca6fb8fb7473f5ae688d11a647672b1b487fd
kotori <lyrapuff@gmail.com>
2024-12-02 20:10:53 +0300

    Feat: ManyError type

--- Kotori.SharedCore/Error.cs
+++ Kotori.SharedCore/Error.cs
@@ -2,6 +2,8 @@ namespace Kotori.SharedCore;
 
 public abstract record Error;
 
+public record ManyError<T>(IReadOnlyCollection<T> Errors) : Error where T : Error;
+
 public record TextError(string Message) : Error
 {
     public static implicit operator TextError(string message)
