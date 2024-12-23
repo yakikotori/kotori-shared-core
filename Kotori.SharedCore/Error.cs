1730977618ce42d9c74a6dd98ee2e8c3cae1e783
kotori <lyrapuff@gmail.com>
2024-12-23 16:44:26 +0300

    Feat: TextError and ManyError ToString implementations

--- Kotori.SharedCore/Error.cs
+++ Kotori.SharedCore/Error.cs
@@ -2,10 +2,17 @@ namespace Kotori.SharedCore;
 
 public abstract record Error;
 
-public record ManyError<T>(IReadOnlyCollection<T> Errors) : Error where T : Error;
+public record ManyError<T>(IReadOnlyCollection<T> Errors) : Error where T : Error
+{
+    public override string ToString()
+        => string.Join(Environment.NewLine, Errors.Select(error => error.ToString()));
+}
 
 public record TextError(string Message) : Error
 {
     public static implicit operator TextError(string message)
         => new(message);
+
+    public override string ToString()
+        => Message;
 }
